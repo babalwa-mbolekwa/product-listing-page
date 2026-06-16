69798742ad8f6d7d3387faddf55e2ed83e40335d
Babalwa01 <babalwambolekwa93@gmail.com>
2026-06-16 12:57:43 +0200

    add remote patterns for images optimizations

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,19 @@
 import type { NextConfig } from "next"
 
-const nextConfig: NextConfig = {}
+// Adding remote patterns for Next.js to optimize images served from loremflickr.com and picsum.photos (from API).
+const nextConfig: NextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "loremflickr.com",
+            },
+            {
+                protocol: "https",
+                hostname: "picsum.photos",
+            }
+        ]
+    }
+}
 
 export default nextConfig
